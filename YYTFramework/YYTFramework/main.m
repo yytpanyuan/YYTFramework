8dc47d022a02221ee75feab64e1dde9e79f3fc1c
yytpanyuan <yytsoon@gmail.com>
2016-09-19 11:35:38 +0800

    add xcode project

---
+++ YYTFramework/YYTFramework/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YYTFramework
+//
+//  Created by yyt on 16/9/19.
+//  Copyright © 2016年 yyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
