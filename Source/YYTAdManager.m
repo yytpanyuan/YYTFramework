cf132a7e6e020eb065e8ffb980c794989baf5b8c
yytpanyuan <yytsoon@gmail.com>
2022-04-04 22:15:34 +0800

    fix ad load time

--- Source/YYTAdManager.m
+++ Source/YYTAdManager.m
@@ -265,7 +265,7 @@
 {
     [self changeBannerAdType];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self createNewBannerAd];
     });
 }
--- Source/YYTFullAdManager.m
+++ Source/YYTFullAdManager.m
@@ -156,7 +156,7 @@
 {
     [self changeFullAdType];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self createNewFullAd];
     });
 }
